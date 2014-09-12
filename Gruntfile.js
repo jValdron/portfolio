e25e2d55786e26d83eb9656c4b5a24ee12e09473
Jason Valdron <jason@dealerinstincts.com>
2014-09-12 18:42:05 +0000

    gruny

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@
           data: {
             config: config,
             environment: target,
-            version: JSON.parse(require('fs').readFileSync(path.join(process.cwd(), 'package.json'))).version
+            version: JSON.parse(require('fs').readFileSync(require('path').join(process.cwd(), 'package.json'))).version
           }
         },
         dist: {
