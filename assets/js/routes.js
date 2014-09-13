7cd3cd32e7574f24b37d01edd19ca3d7bd214438
Jason Valdron <jason@valdron.ca>
2014-09-13 00:42:57 -0300

    version in templates in angular-route

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -21,54 +21,54 @@
 
       // GET /intro - Intro page - Quick overview of the portfolio.
       .when('/intro', {
-        templateUrl: cdn + 'views/pages/intro.html',
+        templateUrl: cdn + 'views/pages/intro.html?v=' + version,
         key: 'intro'
       })
 
       // GET /resume - Resume page - Gets the resume information from the backend.
       .when('/resume', {
-        templateUrl: cdn + 'views/pages/resume.html',
+        templateUrl: cdn + 'views/pages/resume.html?v=' + version,
         controller: 'ResumeCtrl',
         key: 'resume'
       })
 
       // GET /about/me - About page - More information about Jason Valdron.
       .when('/about/me', {
-        templateUrl: cdn + 'views/pages/about/me.html',
+        templateUrl: cdn + 'views/pages/about/me.html?v=' + version,
         controller: 'AboutMeCtrl',
         key: 'about-me'
       })
 
       // GET /about/portfolio - About page - More information about the portfolio.
       .when('/about/portfolio', {
-        templateUrl: cdn + 'views/pages/about/portfolio.html',
+        templateUrl: cdn + 'views/pages/about/portfolio.html?v=' + version,
         key: 'about-portfolio'
       })
 
       // GET /projects - Projects page - Gets a listing of all the projects.
       .when('/projects', {
-        templateUrl: cdn + 'views/pages/projects.html',
+        templateUrl: cdn + 'views/pages/projects.html?v=' + version,
         controller: 'ProjectsCtrl',
         key: 'projects'
       })
 
       // GET /experiences - Experiences page - Gets information from life/volunteer experiences.
       .when('/experiences', {
-        templateUrl: cdn + 'views/pages/experiences.html',
+        templateUrl: cdn + 'views/pages/experiences.html?v=' + version,
         controller: 'ExperiencesCtrl',
         key: 'experiences'
       })
 
       // GET /experience/:id - Experience page - Gets detailled information from one life/volunteer experience.
       .when('/experience/:id', {
-        templateUrl: cdn + 'views/pages/experience-detailled.html',
+        templateUrl: cdn + 'views/pages/experience-detailled.html?v=' + version,
         controller: 'ExperienceDetailledCtrl',
         key: 'experiences'
       })
 
       // GET /errors/404 - Page Not Found.
       .when('/errors/404', {
-        templateUrl: cdn + 'views/errors/404.html',
+        templateUrl: cdn + 'views/errors/404.html?v=' + version,
         key: 'error'
       })
 
